1dddd57008d4c867517255afd706206621cae17d
Brantley <bccragun@yahoo.com>
2025-09-26 17:52:02 -0600

    try/catch added

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,79 +2,134 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db("recipes").collection("recipes").find();
-    result.toArray().then((recipes) => {
+    try {
+        const recipes = await mongodb
+            .getDatabase()
+            .db("recipes")
+            .collection("recipes")
+            .find()
+            .toArray();
+
         res.setHeader("Content-Type", "application/json");
         res.status(200).json(recipes)
-    })
-    .catch((error) => {
-        res.status(500).json({message: error});
-    });
+    
+    } catch (error) {
+        console.error("There was an error when getting all of your recipes: ", error);
+        res.status(500).json({message: error.message || "An error occurred while getting all your recipes"});
+    }
 };
 
 
 const getSingle = async (req, res) => {
-    const recipeId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db("recipes").collection("recipes").find({_id: recipeId});
-    result.toArray().then((recipes) => {
+    try {
+        const recipeId = new ObjectId(req.params.id);
+        const recipe = await mongodb
+            .getDatabase()
+            .db("recipes")
+            .collection("recipes")
+            .findOne({_id: recipeId});
+
+        if (!recipe) {
+            return res.status(404).json({message: "Your requested recipe doesn't exist"});
+        }
+
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(recipes[0])
-    })
-    .catch((error) => {
-        res.status(500).json({message: error});
-    });
+        res.status(200).json(recipe)
+
+    } catch (error) {
+        console.error("There was an error when getting your recipe: ", error);
+        res.status(500).json({message: error.message || "An error occurred while getting your recipe"});
+    }
 };
 
 
 const createRecipe = async (req, res) => {
     // const recipeId = new ObjectId(req.params.id);
-    const recipe = {
-        title: req.body.title,
-        author: req.body.author,
-        cuisine: req.body.cuisine,
-        rating: req.body.rating,
-        ingredients: req.body.ingredients,
-        cook_temp: req.body.cook_temp,
-        cook_time: req.body.cook_time,
-    };
-    const response = await mongodb.getDatabase().db("recipes").collection("recipes").insertOne(recipe);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while creating a recipe");
+    try {
+        const recipe = {
+            title: req.body.title,
+            author: req.body.author,
+            cuisine: req.body.cuisine,
+            rating: req.body.rating,
+            ingredients: req.body.ingredients,
+            cook_temp: req.body.cook_temp,
+            cook_time: req.body.cook_time,
+        };
+        const response = await mongodb
+            .getDatabase()
+            .db("recipes")
+            .collection("recipes")
+            .insertOne(recipe);
+
+        if (response.acknowledged) {
+            return res.status(201).json({
+                message: "Your recipe was created!"
+            });
+        }
+
+    } catch (error) {
+        console.error("There was an error when creating your recipe: ", error);
+        res.status(500).json({message: error.message || "An error occurred while creating your recipe"});
     }
+    
 };
 
 
 const updateRecipe = async (req, res) => {
-    const recipeId = new ObjectId(req.params.id);
-        const recipe = {
-        title: req.body.title,
-        author: req.body.author,
-        cuisine: req.body.cuisine,
-        rating: req.body.rating,
-        ingredients: req.body.ingredients,
-        cook_temp: req.body.cook_temp,
-        cook_time: req.body.cook_time,
-    };
-    const response = await mongodb.getDatabase().db("recipes").collection("recipes").replaceOne({_id: recipeId}, recipe);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while updating the recipe.")
+    try {
+        const recipeId = new ObjectId(req.params.id);
+            const recipe = {
+            title: req.body.title,
+            author: req.body.author,
+            cuisine: req.body.cuisine,
+            rating: req.body.rating,
+            ingredients: req.body.ingredients,
+            cook_temp: req.body.cook_temp,
+            cook_time: req.body.cook_time,
+        };
+        const response = await mongodb
+            .getDatabase()
+            .db("recipes")
+            .collection("recipes")
+            .replaceOne({_id: recipeId}, recipe);
+
+        if (response.matchedCount === 0) {
+            return res.status(404).json({
+                message: "Your recipe was not found."
+            });
+        }
+
+        return res.status(200).json({message: "Your recipe was updated!"});
+
+    } catch (error) {
+        console.error("There was an error when updating your recipe: ", error);
+        res.status(500).json({message: error.message || "An error occurred when updating your recipe"});
     }
+
 };
 
 
 const deleteRecipe = async (req, res) => {
-    const recipeId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db("recipes").collection("recipes").deleteOne({_id: recipeId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while deleting the recipe.")
+    try {
+        const recipeId = new ObjectId(req.params.id);
+        const response = await mongodb
+            .getDatabase()
+            .db("recipes")
+            .collection("recipes")
+            .deleteOne({_id: recipeId});
+
+        if (response.deletedCount === 0) {
+            return res.status(404).json({message: "Your recipe was not found."});
+        }
+
+        res.status(200).json({message: "Your recipe was successfully deleted."});
+
+    } catch (error) {
+        console.error("There was an error when deleting your recipe: ", error);
+        res.status(500).json({message: error.message || "An error occurred while deleting the recipe."});
     }
-}
+    
+};
 
 
 
--- controllers/users.js
+++ controllers/users.js
@@ -2,69 +2,121 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db("users").collection("users").find();
-    result.toArray().then((users) => {
+    try {
+        const users = await mongodb
+            .getDatabase()
+            .db("users")
+            .collection("users")
+            .find()
+            .toArray();
+
         res.setHeader("Content-Type", "application/json");
         res.status(200).json(users)
-    })
-    .catch((error) => {
-        res.status(500).json({message: error});
-    });
+        
+    } catch (error) {
+        console.error("There was an error when getting all of your users: ", error);
+        res.status(500).json({message: error.message || "An error occurred when getting your contacts."})
+    }
 };
 
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db("users").collection("users").find({_id: userId});
-    result.toArray().then((users) => {
+    try {
+        const userId = new ObjectId(req.params.id);
+        const user = await mongodb
+            .getDatabase()
+            .db("users")
+            .collection("users")
+            .findOne({_id: userId});
+            
+            if (!user) {
+                return res.status(404).json({message: "Your requested user doesn't exist"});
+            }
+            
         res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users[0])
-    })
-    .catch((error) => {
-        res.status(500).json({message: error});
-    });
+        res.status(200).json(user)
+
+    } catch (error) {
+        console.error("There was an error when getting your user: ", error);
+        res.status(500).json({message: error.message || "An error occurred when getting your user"});
+    }
 };
 
 
 const createUser = async (req, res) => {
     // const userId = new ObjectId(req.params.id);
-    const user = {
-        name: req.body.name,
-        age: req.body.age,
-    };
-    const response = await mongodb.getDatabase().db("users").collection("users").insertOne(user);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while creating a user");
+    try {
+        const user = {
+            name: req.body.name,
+            age: req.body.age,
+        };
+        const response = await mongodb
+            .getDatabase()
+            .db("users")
+            .collection("users")
+            .insertOne(user);
+
+        if (response.acknowledged) {
+            return res.status(201).json({
+                message: "Your user was created!"
+            });
+        }
+
+    } catch (error) {
+        console.error("There was an error when creating your user: ", error);
+        res.status(500).json({message: error.message || "An error occurred when creating your user"});
     }
 };
 
 
 const updateUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-        const user = {
-        name: req.body.name,
-        age: req.body.age,
-    };
-    const response = await mongodb.getDatabase().db("users").collection("users").replaceOne({_id: userId}, user);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while updating the user.")
+    try {
+        const userId = new ObjectId(req.params.id);
+            const user = {
+            name: req.body.name,
+            age: req.body.age,
+        };
+        const response = await mongodb
+            .getDatabase()
+            .db("users")
+            .collection("users")
+            .replaceOne({_id: userId}, user);
+
+        if (response.matchedCount === 0) {
+            return res.status(404).json({
+                message: "Your user was not found."
+            });
+        }
+
+        return res.status(200).json({message: "Your user was updated!"});
+
+    } catch (error) {
+        console.error("There was an error when updating your user: ", error);
+        res.status(500).json({message: error.message || "An error occurred when updating your user"});
     }
 };
 
 
 const deleteUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db("users").collection("users").deleteOne({_id: userId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "An error occured while deleting the user.")
+    try {
+        const userId = new ObjectId(req.params.id);
+        const response = await mongodb
+            .getDatabase()
+            .db("users")
+            .collection("users")
+            .deleteOne({_id: userId});
+
+        if (response.deletedCount === 0) {
+            return res.status(404).json({message: "Your user was not found."});
+        }
+
+        res.status(200).json({message: "Your user was successfully deleted."})
+
+    } catch (error) {
+        console.error("There was an error when deleting your user: ", error)
+        res.status(500).json({message: error.message || "An error occurred when deleting your user."});
     }
-}
+};
 
 
 
