770e27667e11dacd36b15c4a0c05f7227f8bdb6c
Brantley <bccragun@yahoo.com>
2025-09-27 09:48:48 -0600

    oauth config

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const recipesController = require("../controllers/recipes");
 const {validate, createRecipeRules, updateRecipeRules, getRecipeByIdRules, deleteRecipeByIdRules} = require("../middleware/validation");
+const {isAuthenticated} = require("../middleware/authenticate");
+
 
 router.get("/", recipesController.getAll);
 router.get("/:id", getRecipeByIdRules(), validate, recipesController.getSingle);
 router.post("/", createRecipeRules(), validate, recipesController.createRecipe);
-router.put("/:id", updateRecipeRules(), validate, recipesController.updateRecipe);
-router.delete("/:id", deleteRecipeByIdRules(), validate, recipesController.deleteRecipe);
+router.put("/:id", isAuthenticated, updateRecipeRules(), validate, recipesController.updateRecipe);
+router.delete("/:id", isAuthenticated, deleteRecipeByIdRules(), validate, recipesController.deleteRecipe);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 const {validate, createUserRules, updateUserRules, getUserByIdRules, deleteUserByIdRules} = require("../middleware/validation");
+const {isAuthenticated} = require("../middleware/authenticate");
 
 router.get("/", usersController.getAll);
 router.get("/:id", getUserByIdRules(), validate, usersController.getSingle);
 router.post("/", createUserRules(), validate, usersController.createUser);
-router.put("/:id", updateUserRules(), validate, usersController.updateUser);
-router.delete("/:id", deleteUserByIdRules(), validate, usersController.deleteUser);
+router.put("/:id", isAuthenticated, updateUserRules(), validate, usersController.updateUser);
+router.delete("/:id", isAuthenticated, deleteUserByIdRules(), validate, usersController.deleteUser);
 
 module.exports = router;
